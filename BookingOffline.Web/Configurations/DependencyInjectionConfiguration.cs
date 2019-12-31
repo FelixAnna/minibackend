650c8d561597e4f467a8cab47823cadbf9b6cbc0
FelixAnna <123456a>
2019-12-31 15:12:45 +0800

    migrate settings to configuration

--- BookingOffline.Web/Configurations/DependencyInjectionConfiguration.cs
+++ BookingOffline.Web/Configurations/DependencyInjectionConfiguration.cs
@@ -3,6 +3,7 @@ using BookingOffline.Repositories;
 using BookingOffline.Repositories.Interfaces;
 using BookingOffline.Services;
 using BookingOffline.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BookingOffline.Web.Configurations
@@ -21,13 +22,13 @@ namespace BookingOffline.Web.Configurations
         {
             services.AddScoped<IAlipayUserRepository, AlipayUserRepository>();
             services.AddScoped<IOrderRepository, OrderRepository>();
-            services.AddScoped<IOrderItemRepository, OrderItemRepository>(); 
+            services.AddScoped<IOrderItemRepository, OrderItemRepository>();
             services.AddScoped<SQLiteDBContext>();
         }
 
-        public static void AddCommonSevices(this IServiceCollection services)
+        public static void AddCommonSevices(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddScoped<IAlipayService, AlipayService>();
+            services.AddScoped<IAlipayService, AlipayService>(provider => new AlipayService(configuration["alipay::appId"], configuration["alipay::privateKey"], configuration["alipay::publicKey"]));
             services.AddScoped<ITokenGeneratorService, TokenGeneratorService>();
         }
     }
--- BookingOffline.Web/Startup.cs
+++ BookingOffline.Web/Startup.cs
@@ -28,7 +28,7 @@ namespace BookingOffline.Web
 
             services.AddBSSevices();
             services.AddDASevices();
-            services.AddCommonSevices();
+            services.AddCommonSevices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
