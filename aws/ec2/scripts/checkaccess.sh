ff97b818578ff310c8b90983800673e848e947ec
yufelix <yuecnu@hotmail.com>
2020-05-17 00:41:19 +0800

    try fix startup issue

--- aws/ec2/scripts/checkaccess.sh
+++ aws/ec2/scripts/checkaccess.sh
@@ -1,3 +1,15 @@
 #!/bin/bash  
 set -e  
-wget http://127.0.0.1:5000/test/running
+
+
+index=6
+while [ $index -gt 0 ]
+do
+	sleep 5s
+	response=$(curl http://127.0.0.1:5000/test/running)
+	if grep -q "succeed" <<< "$response"; then
+		echo $response
+		index=0
+	fi
+	index=$(($index-1))
+done
