cf1937c1b969b7f893ba7c25f5e2b1c8a331ce55
yufelix <yuecnu@hotmail.com>
2020-05-18 22:41:36 +0800

    try fix startup issue

--- aws/ec2/scripts/start_server.sh
+++ aws/ec2/scripts/start_server.sh
@@ -5,10 +5,10 @@ cd /webapps/boapi/
 sudo pgrep -f "dotnet BookingOffline.Web.dll"
 sudo pkill -f "dotnet BookingOffline.Web.dll"
 
-dotnet dev-certs https --clean
-dotnet dev-certs https --verbose
+#dotnet dev-certs https --clean
+#dotnet dev-certs https --verbose
 
 sudo touch startup.log
 sudo chmod 777 startup.log
 
-sudo dotnet BookingOffline.Web.dll --urls=https://*:443 >>startup.log 2>&1 &
+sudo dotnet BookingOffline.Web.dll --urls=http://*:80 >>startup.log 2>&1 &
